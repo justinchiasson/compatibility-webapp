17b008f889102ebaf6523b0acea0e8d8d3e21181
JasperDeasey <60531248+JasperDeasey@users.noreply.github.com>
2021-01-30 13:14:08 -0800

    added sorting functionality; cleaned up code

--- backend/apiCommunicator.js
+++ backend/apiCommunicator.js
@@ -1,19 +1,18 @@
 import * as http from 'http';
 
 export async function getItems(keyword) {
-    console.log("getItems function started")
-    let json = await searchAPI(keyword)
-    return parseJSON(json)
+    return await searchAPI(keyword)
 }
 
 async function searchAPI(keyword) {
-    console.log("searchAPI function started")
-    let apiKey = "bkFXAolb3phZVMGX1yhl6uDQ"
     let baseURL = "https://api.bestbuy.com/v1/products"
-    keyword = formatKeyword(keyword)
-    let url = baseURL+"("+keyword+")?apiKey="+apiKey+"&format=json"
+    let formattedKeyword = formatKeyword(keyword)
+    let apiKey = "?apiKey=bkFXAolb3phZVMGX1yhl6uDQ"
+    let sortOptions = "&sort=customerReviewCount.dsc"
+    let showOptions = "&show=name,regularPrice,shortDescription,thumbnailImage,url,customerReviewCount"
+    let responseFormat = "&format=json"
+    let url = baseURL+formattedKeyword+apiKey+sortOptions+showOptions+responseFormat
     return new Promise((resolve, reject) => {
-        console.log("in the new Primise (resolve, reject)... thing")
         http.get(url, (res) => {
             res.setEncoding("utf8");
             let data = "";
@@ -30,34 +29,12 @@ async function searchAPI(keyword) {
                 }
             });
         }).on("error", (e) => {
-            console.log("error in http.get = "+e)
             return resolve();
         });
     });
 }
 
 function formatKeyword(keyword) {
-    return "search="+keyword.replace(/\s+/g, '&search=')
-}
-
-function parseJSON(json) {
-    let productList = json["products"]
-    let jsonStr = '{"products":[]}';
-    let revisedJSONList = JSON.parse(jsonStr);
-    productList.forEach((product) => {
-        try {
-            let jsonItem = {}
-            jsonItem.name = product.name
-            jsonItem.price = product.regularPrice
-            jsonItem.url = product.url
-            jsonItem.description = product.shortDescription
-            jsonItem.images = product.images
-            revisedJSONList['products'].push(jsonItem);
-        } catch (e) {
-            console.log("error with jsonItem: " + e)
-        }
-    })
-    console.log(revisedJSONList)
-    return revisedJSONList
+    return "((search="+keyword.replace(/\s+/g, '&search=')+"))"
 }
 
